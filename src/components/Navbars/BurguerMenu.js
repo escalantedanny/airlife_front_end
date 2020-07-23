f6f9012a5aa650845bacf014ec246fb0b4c66361
Danny Escalante <danny.ezequiel@gmail.com>
2020-07-23 02:58:42 -0400

    estilos menu letra empresarial
    estilos menu letra empresarial

--- src/components/Navbars/BurguerMenu.js
+++ src/components/Navbars/BurguerMenu.js
@@ -61,15 +61,15 @@ const BurguerMenu = () => {
         right
         styles={ styles } 
       >
-        <a target="_blank" id="nosotros" className="menu-item link-nosotros" href="http://airlife.com/nosotros/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Empresa &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <a target="_blank" id="nosotros" className="menu-item link-nosotros" href="http://airlife.com/nosotros/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Empresa &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
         <a target="_blank" id="tecnologia" className="menu-item link-tecnologia" href="/">&nbsp;&nbsp;&nbsp;&nbsp;Tecnolog&iacute;a &nbsp;&nbsp;&nbsp;</a>
-        <a target="_blank" id="certificaciones" className="menu-item link-certificaciones" href="/about">&nbsp;Certificaciones&nbsp;</a>
-        <a target="_blank" id="transporte" className="menu-item link-transport" href="/">&nbsp;&nbsp;&nbsp;&nbsp;Transporte&nbsp;&nbsp;&nbsp;</a>
-        <a target="_blank" id="business" className="menu-item link-business" href="http://airlife.com/business/">&nbsp;&nbsp;&nbsp;&nbsp;Businesses&nbsp;&nbsp;</a>
-        <a target="_blank" id="health" className="menu-item link-health" href="http://airlife.com/health/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Health&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-        <a target="_blank" id="home" className="menu-item link-home" href="http://airlife.com/home/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <a target="_blank" id="certificaciones" className="menu-item link-certificaciones" href="/about">Certificaciones</a>
+        <a id="transporte" className="menu-item link-transport" href="/">&nbsp;&nbsp;&nbsp; Transporte &nbsp;&nbsp;&nbsp;</a>
+        <a target="_blank" id="business" className="menu-item link-business" href="http://airlife.com/business/">&nbsp;&nbsp;&nbsp;&nbsp; Businesses&nbsp;&nbsp;</a>
+        <a target="_blank" id="health" className="menu-item link-health" href="http://airlife.com/health/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Health&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <a target="_blank" id="home" className="menu-item link-home" href="http://airlife.com/home/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
         <a target="_blank" id="publi_spaces" className="menu-item link-publicspaces" href="http://airlife.com/public_spaces/">&nbsp;&nbsp;Public spaces</a>
-        <a target="_blank" id="industrial" className="menu-item link-industrial" href="http://airlife.com/industrial/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Industrial &nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <a target="_blank" id="industrial" className="menu-item link-industrial" href="http://airlife.com/industrial/">&nbsp;&nbsp;&nbsp;&nbsp;Industrial &nbsp;&nbsp;&nbsp;&nbsp;</a>
 
       </Menu>
     );
