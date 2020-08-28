25a830a422ddc39c1cfb258a76d6209712924b9e
Danny Escalante <danny.ezequiel@gmail.com>
2020-08-28 12:19:11 -0400

    carpeta build

---
+++ build/precache-manifest.ea921e50589e3c11229ea607a9460c28.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b33b35fc1f9e093679da243ee5a581b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f004b9794086365f841c",
+    "url": "/static/css/main.f6a8d4c8.chunk.css"
+  },
+  {
+    "revision": "5c070b332a70820f96ac",
+    "url": "/static/js/2.40e5d553.chunk.js"
+  },
+  {
+    "revision": "e1f37b9e4c4a4927a03343b0647e3521",
+    "url": "/static/js/2.40e5d553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f004b9794086365f841c",
+    "url": "/static/js/main.1c5c718c.chunk.js"
+  },
+  {
+    "revision": "20c9adbcea5ae881b263",
+    "url": "/static/js/runtime-main.9f396aa9.js"
+  },
+  {
+    "revision": "ce0b349394d421d4e596026acc29f438",
+    "url": "/static/media/1_1.ce0b3493.png"
+  },
+  {
+    "revision": "e8bdc49f390c01055f6fecb2e99fa7e6",
+    "url": "/static/media/2.e8bdc49f.png"
+  },
+  {
+    "revision": "8afed2fc40b51f3ce4098fe2029c7ee9",
+    "url": "/static/media/3.8afed2fc.png"
+  },
+  {
+    "revision": "e328e07169eadbae5cb60367557e45b8",
+    "url": "/static/media/7.e328e071.png"
+  },
+  {
+    "revision": "1fdef21b0cf68c24db43171878ad4d01",
+    "url": "/static/media/ITC - OfficinaSansITCStd-Bold.1fdef21b.otf"
+  },
+  {
+    "revision": "fa9d209ba37c4b479bdc93db9d505122",
+    "url": "/static/media/ITC - OfficinaSansITCStd-Book.fa9d209b.otf"
+  },
+  {
+    "revision": "1af8c99a1ddf86f90b7846726ba83b01",
+    "url": "/static/media/auto_tapaboca.1af8c99a.png"
+  },
+  {
+    "revision": "032b8bbb03d47ffd9a5c98ccbc68664a",
+    "url": "/static/media/baner_final.032b8bbb.png"
+  },
+  {
+    "revision": "bdd2ed6d5f250b609f7bc26bd0362486",
+    "url": "/static/media/beneficio2.bdd2ed6d.png"
+  },
+  {
+    "revision": "538a7e1b7f452c92b2918fbe1f0cc4c8",
+    "url": "/static/media/beneficio3.538a7e1b.png"
+  },
+  {
+    "revision": "c7c72660f82e3ce2f588a1d05187f263",
+    "url": "/static/media/beneficio4.c7c72660.png"
+  },
+  {
+    "revision": "f63f0ba5465ab0dd4cf6ca52137f029f",
+    "url": "/static/media/carro_final.f63f0ba5.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3d84f6ec1ddf5e04cfcab47d359b2823",
+    "url": "/static/media/infografia2.3d84f6ec.png"
+  },
+  {
+    "revision": "8a6a766a760de1bfec4152e63015e8e7",
+    "url": "/static/media/pide_airlife.8a6a766a.png"
+  },
+  {
+    "revision": "b54bf97bbbfe3142088c3ce20b4ec344",
+    "url": "/static/media/sello_airlife.b54bf97b.png"
+  }
+]);
